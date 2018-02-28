753dc8539c19ee361002df44cc603a1300bb016a
ri616 <ri616@ic.ac.uk>
2018-02-28 23:07:51 +0000

    added convert_to_codes function

--- decompressor.cpp
+++ decompressor.cpp
@@ -11,36 +11,55 @@
 
 using namespace std;
 
-void print_string(string input); // remove
+static void convert_to_codes(string str, int* codes);
 
 int main(int argc, char **argv)
 {
-  if (argc < 2) {
+  if (argc < 2)
+  {
     cerr << "No filename given" << endl;
     exit(EXIT_FAILURE);
   }
 
   ifstream file(argv[1]);
-  if (!file) {
+  if (!file)
+  {
     cerr << "Unknown filename" << endl;
     exit(EXIT_FAILURE);
   }
 
   stringstream content;
   content << file.rdbuf();
+  file.close();
+
   assert((content.str().length() * BITS_PER_BYTE) % CODE_WIDTH == 0); // remove
 
-  //cout << content.str().length() << endl;
-  //print_string(content.str());
-  file.close();
+  int arr_len = (content.str().length() / 3) * 2;
+  int codes[arr_len];
+  convert_to_codes(content.str(), codes);
+
+  for (int i=0; i<arr_len; i++) {
+    cout << codes[i] << " ";
+  }
+  cout << endl;
 
   return 0;
 }
 
-void print_string(string input)
+static void convert_to_codes(string str, int* codes)
 {
-  for (int i=0; i<input.length(); i++)
+  int cur_pos = 0;
+  for (int i=0; i<str.length(); i+=3)
   {
-    cout << input.at(i) << endl;
+    int c1 = (int) str.at(i);
+    char c2 = str.at(i+1);
+    int c3 = (int) str.at(i+2);
+
+    int lower = c2 & 0x0F;
+    int upper = (c2 << 4) & 0x00000F00;
+
+    codes[cur_pos] = (c1 << 4) | lower;
+    codes[cur_pos+1] = c3 | upper;
+    cur_pos += 2;
   }
 }
